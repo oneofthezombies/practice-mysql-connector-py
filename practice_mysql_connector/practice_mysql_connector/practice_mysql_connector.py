087930bc4286738087b7b0e356a753cf02842d33
oneofthezombies <hunhoekim@gmail.com>
2018-10-22 18:09:34 +0900

    add: ~ join

---
+++ practice_mysql_connector/practice_mysql_connector/practice_mysql_connector.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="oneofthezombies"
+)
+
+mycursor = mydb.cursor()
+
+#mycursor.execute("CREATE DATABASE opentutorials")  # opentutorials = database name
+#mycursor.execute("DROP DATABASE opentutorials")
+#mycursor.execute("SHOW DATABASES")
+#mycursor.execute("USE opentutorials")
+#mycursor.execute("SET PASSWORD = PASSWORD('oneofthezombies')") # oneofthezombies = password
+#mycursor.execute("exit")
+#mycursor.execute("SHOW TABLES")
+#mycursor.execute("DESC topic") # topic = table name
+#mycursor.execute("INSERT INTO topic (title,description,created,author,profile) VALUES('MySQL','MySQL is ...',NOW(),'egoing','developer'")  # MySQL = title, My SQL is ... = description, NOW() = created, egoing = author, developer = prifle
+#mycursor.execute("SELECT * FROM topic")
+#mycursor.execute("SELECT id, title, created, author FROM topic")
+#mycursor.execute("SELECT id, title, created, author FROM topic WHERE author='egoing'")
+#mycursor.execute("SELECT id, title, created, author FROM topic WHERE author='egoing' ORDER BY id DESC LIMIT 2")
+#mycursor.execute("RENAME TABLE topic TO topic_backup")
+#mycursor.execute("SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.id")
+#mycursor.execute("SELECT topic.id, title, description, created, name, profile FROM topic LEFT JOIN author ON topic.author_id = author.id")
